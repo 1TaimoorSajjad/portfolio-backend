5d3c99ad993ad74a097052f356d17d07e3ecc8b2
1taimoorsajjad <taimoorsajjad980@gmail.com>
2023-10-28 22:12:46 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const cors = require('cors'); // Import the cors middleware
+
+
+const app = express();
+const port = 3000; // Choose the port you want
+
+app.use(bodyParser.json());
+app.use(cors());
+
+
+const transporter = nodemailer.createTransport({
+  service: 'Gamil', // e.g., 'Gmail'
+  auth: {
+    user: 'taimoorsajjad980@gmail.com',
+    pass: 'my new password is very strong@123'
+  }
+});
+
+app.post('/send-email', (req, res) => {
+  const { name, email, message } = req.body;
+
+  const mailOptions = {
+    from: 'taimoorsajjad980@gmail.com',
+    to: 'taimoorsajjad980@gmail.com', // Replace with the recipient's email
+    subject: 'Contact Form Submission',
+    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error sending email:', error);
+      res.status(500).send('Error sending email');
+    } else {
+      console.log('Email sent:', info.response);
+      res.send('Email sent successfully');
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
