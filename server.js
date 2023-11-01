8bc9a6416107f97d7409ee5c4df94fb49d408eb2
1taimoorsajjad <taimoorsajjad980@gmail.com>
2023-11-01 22:41:44 +0500

    changes in the server side code

--- server.js
+++ server.js
@@ -1,45 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
-const cors = require('cors'); // Import the cors middleware
 
-
-const app = express();
-const port = 3000; // Choose the port you want
-
-app.use(bodyParser.json());
-app.use(cors());
-
-
+// Create a transporter object using your email service provider's SMTP settings.
 const transporter = nodemailer.createTransport({
-  service: 'Gmail', // e.g., 'Gmail'
+  service: 'Gmail',
   auth: {
     user: 'taimoorsajjad980@gmail.com',
     pass: 'my new password is very strong@123'
   }
 });
 
-app.post('/send-email', (req, res) => {
-  const { name, email, message } = req.body;
+// Define your email content.
+const mailOptions = {
+  from: 'your_email@gmail.com',
+  to: 'taimoorsajjad980@gmail.com', // Receiver's email address
+  subject: 'New Contact Form Submission',
+  text: 'You have a new contact form submission on your website.',
+};
 
-  const mailOptions = {
-    from: 'taimoorsajjad980@gmail.com',
-    to: 'taimoorsajjad980@gmail.com', // Replace with the recipient's email
-    subject: 'Contact Form Submission',
-    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
-  };
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.error('Error sending email:', error);
-      res.status(500).send('Error sending email');
-    } else {
-      console.log('Email sent:', info.response);
-      res.send('Email sent successfully');
-    }
-  });
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+// Send the email.
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    console.error('Error sending email:', error);
+  } else {
+    console.log('Email sent:', info.response);
+  }
 });
